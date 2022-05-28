e2162d99ac20155df1f7b0d69b552a84377c8b14
kyomin <kim031504@naver.com>
2022-05-28 21:14:16 +0900

    API 함수 타입 정의

--- vue-news/src/api/index.ts
+++ vue-news/src/api/index.ts
@@ -1,45 +1,52 @@
-import axios from 'axios';
+import axios, { AxiosPromise } from 'axios';
 
 const api = {
-  news: 'https://api.hnpwa.com/v0/news/1.json',
-  ask: 'https://api.hnpwa.com/v0/ask/1.json',
-  jobs: 'https://api.hnpwa.com/v0/jobs/1.json',
-  user: 'https://api.hnpwa.com/v0/user/',
-  item: 'https://api.hnpwa.com/v0/item/'
+	news: 'https://api.hnpwa.com/v0/news/1.json',
+	ask: 'https://api.hnpwa.com/v0/ask/1.json',
+	jobs: 'https://api.hnpwa.com/v0/jobs/1.json',
+	user: 'https://api.hnpwa.com/v0/user/',
+	item: 'https://api.hnpwa.com/v0/item/',
 };
 
-function fetchNews() {
-  return axios.get(api.news);
+interface NewsItem {
+	comments_count: number;
+	domain: string;
+	id: number;
+	points: number;
+	time: number;
+	time_ago: string;
+	title: string;
+	type: string;
+	url: string;
+	user: string;
+}
+
+// AxiosPromise<T>에서 T는 response.data에 담겨올 타입을 넘긴다.
+function fetchNews(): AxiosPromise<NewsItem[]> {
+	return axios.get(api.news);
 }
 
 function fetchAsk() {
-  return axios.get(api.ask);
+	return axios.get(api.ask);
 }
 
 function fetchJobs() {
-  return axios.get(api.jobs);
+	return axios.get(api.jobs);
 }
 
 function fetchUser(id: any) {
-  const url = `${api.user}${id}.json`;
-  return axios.get(url);
+	const url = `${api.user}${id}.json`;
+	return axios.get(url);
 }
 
 function fetchItem(id: any) {
-  const url = `${api.item}${id}.json`;
-  return axios.get(url);
+	const url = `${api.item}${id}.json`;
+	return axios.get(url);
 }
 
 function fetchList(type: any) {
-  const url = `https://api.hnpwa.com/v0/${type}/1.json`;
-  return axios.get(url);
+	const url = `https://api.hnpwa.com/v0/${type}/1.json`;
+	return axios.get(url);
 }
 
-export {
-  fetchNews,
-  fetchAsk,
-  fetchJobs,
-  fetchUser,
-  fetchItem,
-  fetchList,
-}
+export { fetchNews, fetchAsk, fetchJobs, fetchUser, fetchItem, fetchList };
--- vue-news/src/views/ListView.vue
+++ vue-news/src/views/ListView.vue
@@ -19,6 +19,11 @@ export default {
   },
   methods: {
     async fetchNewsItems() {
+      /* 
+        이제 여기서 response.data는 NewsItem[] 타입임을 안다.
+        따라서 response.data는 배열의 메서드를 호출할 수 있고,
+        response.data[0]은 NewsItem의 속성에 접근할 수 있도록 자동 완성이 된다.
+      */
       const response = await fetchNews();
       this.newsItems = response.data;
     },
