710d23557be6e8068ac1ef25ca734929f154fc0a
kyomin <kim031504@naver.com>
2022-06-15 22:51:42 +0900

    차트 플러그인 타입 에러 해결

--- vue-chart/src/App.vue
+++ vue-chart/src/App.vue
@@ -6,10 +6,15 @@
 
 <script lang="ts">
 import Vue from "vue";
-import Chart from "chart.js/auto";
+// import Chart from "chart.js/auto";
 
 export default Vue.extend({
   mounted() {
+    const canvasElement = document.getElementById(
+      "myChart"
+    ) as HTMLCanvasElement;
+    const ctx = canvasElement.getContext("2d");
+
     const labels = ["January", "February", "March", "April", "May", "June"];
 
     const data = {
@@ -30,10 +35,11 @@ export default Vue.extend({
       options: {},
     };
 
-    const myChart = new Chart(
-      document.getElementById("myChart") as HTMLCanvasElement,
-      config
-    );
+    // null 타입 가드
+    if (!ctx) {
+      return;
+    }
+    const myChart = new this.$_Chart(ctx, config);
   },
 });
 </script>
--- vue-chart/src/main.ts
+++ vue-chart/src/main.ts
@@ -1,8 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import App from './App.vue';
+import ChartPlugin from './plugins/ChartPlugin';
 
+/*
+  Vue.use를 취함으로써
+  ChartPlugin 안에 있는 install의 내부 코드가 실행된다.
+
+  뷰엑스, 뷰라우터도 동일한 원리로 플러그인이 주입되는 것이다.
+*/
+Vue.use(ChartPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+	render: (h) => h(App),
+}).$mount('#app');
